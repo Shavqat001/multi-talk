b23d309e2c1b1ec27231e1b40c1f65a0e5ac9ede
Shavqat <71512034+Shavqat001@users.noreply.github.com>
2024-09-19 15:18:47 +0500

    Update theme.js

--- public/js/theme.js
+++ public/js/theme.js
@@ -1,7 +1,44 @@
-const themeToggle = document.querySelector('.page__theme');
+let themeToggle = document.querySelector('.page__theme');
 let usersAside = document.querySelector('.users');
+let pageSettings = document.querySelector('.page__settings');
+let blackLayer = document.querySelector('.messages__black-layer');
+
+let pageMenu = document.querySelector('.users__settings-menu');
+let closeButton = document.querySelector('.page__settings--close-button');
+
+let smileContent = document.querySelector('.messages__smile-icons');
+
+let smiles = smileContent.children;
+
+let smilesArr = ['😂', '👍', '😡'];
+
+for (let i = 0; i < smiles.length; i++) {
+    smiles[i].addEventListener('click', () => {
+        messageInput.value += smilesArr[i];
+        messageInput.focus();
+    });
+}
 
 let isLight = JSON.parse(localStorage.getItem('theme'));
+
+let isOpened = false;
+
+pageMenu.addEventListener('click', () => {
+    blackLayer.classList.add('messages__black-layer--opened');
+    pageSettings.classList.add('page__settings--opened');
+});
+
+closeButton.addEventListener('click', () => {
+    blackLayer.classList.remove('messages__black-layer--opened');
+    pageSettings.classList.remove('page__settings--opened');
+});
+
+window.addEventListener('keydown', (e) => {
+    if (e.key = 'Esc') {
+        blackLayer.classList.remove('messages__black-layer--opened');
+        pageSettings.classList.remove('page__settings--opened');
+    }
+})
 
 themeToggle.addEventListener('click', () => {
     if (isLight) {
@@ -12,6 +49,8 @@ themeToggle.addEventListener('click', () => {
         messagesContent.classList.remove('messages--light');
         document.querySelectorAll('.messages__item').forEach(el => el.classList.remove('messages__item--light'));
         themeToggle.classList.remove('page__theme--light');
+        pageSettings.classList.remove('page__settings--light');
+        pageMenu.classList.remove('users__settings-menu--light');
         isLight = false;
         JSON.stringify(localStorage.setItem('theme', isLight))
     } else {
@@ -22,6 +61,8 @@ themeToggle.addEventListener('click', () => {
         messagesContent.classList.add('messages--light');
         document.querySelectorAll('.messages__item').forEach(el => el.classList.add('messages__item--light'));
         themeToggle.classList.add('page__theme--light');
+        pageSettings.classList.add('page__settings--light');
+        pageMenu.classList.add('users__settings-menu--light');
         isLight = true;
         JSON.stringify(localStorage.setItem('theme', isLight))
     }
@@ -37,6 +78,8 @@ window.addEventListener('load', () => {
             messagesContent.classList.add('messages--light');
             document.querySelectorAll('.messages__item').forEach(el => el.classList.add('messages__item--light'));
             themeToggle.classList.add('page__theme--light');
+            pageMenu.classList.add('users__settings-menu--light');
+            pageSettings.classList.add('page__settings--light');
         } else {
             usersAside.classList.remove('users__light--theme');
             document.querySelectorAll('.users__item').forEach(el => el.classList.remove('users__item--light'));
@@ -44,7 +87,9 @@ window.addEventListener('load', () => {
             form.classList.remove('messages__form--light');
             messagesContent.classList.remove('messages--light');
             document.querySelectorAll('.messages__item').forEach(el => el.classList.remove('messages__item--light'));
+            pageMenu.classList.remove('users__settings-menu--light');
             themeToggle.classList.remove('page__theme--light');
+            pageSettings.classList.remove('page__settings--light');
         }
     }, 100);
 });
